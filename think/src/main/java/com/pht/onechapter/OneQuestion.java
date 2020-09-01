53681845cc00ea4654ea19f5cfd82a755b3970e0
956077081 <956077081@qq.com>
2020-09-01 23:41:04 +0800

    ``

---
+++ think/src/main/java/com/pht/onechapter/OneQuestion.java
@@ -0,0 +1,12 @@
+package com.pht.onechapter;
+
+public class OneQuestion {
+    int age;
+    char  firstWord;
+    public static void main(String[] args) {
+        OneQuestion oneQuestion = new OneQuestion();
+        System.out.println(oneQuestion.age);
+        System.out.println(oneQuestion.firstWord);
+        // 0   ''
+    }
+}
---
+++ think/src/main/java/com/pht/onechapter/TwoQuestion.java
@@ -0,0 +1,6 @@
+package com.pht.onechapter;
+public class TwoQuestion {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
